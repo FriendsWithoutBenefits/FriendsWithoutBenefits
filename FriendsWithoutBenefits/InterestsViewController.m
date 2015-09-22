cba00034be438322bd840c0aea402447697258a8
Jeffrey Jacka <jtjacka@gmail.com>
2015-09-22 15:25:36 -0700

    begin interests

---
+++ FriendsWithoutBenefits/InterestsViewController.m
@@ -0,0 +1,37 @@
+//
+//  InterestsViewController.m
+//  FriendsWithoutBenefits
+//
+//  Created by Jeffrey Jacka on 9/22/15.
+//  Copyright © 2015 Jeffrey Jacka. All rights reserved.
+//
+
+#import "InterestsViewController.h"
+
+@interface InterestsViewController ()
+
+@end
+
+@implementation InterestsViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
