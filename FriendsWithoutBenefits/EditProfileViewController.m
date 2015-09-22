c5285318c450b40d71cff970eae634b31f13d5c3
Matthew Loh <mattloh111@gmail.com>
2015-09-21 23:17:26 -0700

    Working on edit user info

--- FriendsWithoutBenefits/EditProfileViewController.m
+++ FriendsWithoutBenefits/EditProfileViewController.m
@@ -8,16 +8,46 @@
 
 #import "EditProfileViewController.h"
 #import "User.h"
+#import <UIKit/UIKit.h>
 
-@interface EditProfileViewController ()
-
+@interface EditProfileViewController () <UIImagePickerControllerDelegate, UINavigationControllerDelegate>
+@property (strong, nonatomic) UIAlertController *imageSelector;
+@property (strong, nonatomic) UIImagePickerController *picker;
 @end
 
 @implementation EditProfileViewController
 
 - (void)viewDidLoad {
-    [super viewDidLoad];
+  [super viewDidLoad];
+
+  [self setUpActionSheet];
+
     // Do any additional setup after loading the view.
+}
+- (IBAction)editProfilePictureButtonPressed:(UIButton *)sender {
+  self.imageSelector.modalPresentationStyle = UIModalPresentationPageSheet;
+  [self presentViewController:self.imageSelector animated:true completion:nil];
+}
+
+- (void)setUpActionSheet {
+  self.picker = [[UIImagePickerController alloc] init];
+  self.picker.delegate = self;
+  self.imageSelector = [UIAlertController alertControllerWithTitle:@"Image Source" message:@"Select image from: " preferredStyle:UIAlertControllerStyleActionSheet];
+  UIAlertAction *cancelAction = [UIAlertAction actionWithTitle:@"Cancel" style:UIAlertActionStyleDestructive handler:nil];
+  UIAlertAction *chooseImageFromPhotoLibrary = [UIAlertAction actionWithTitle:@"Photo Library" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+    self.picker.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+    [self presentViewController:self.picker animated:true completion:nil];
+  }];
+  UIAlertAction *takePicture = [UIAlertAction actionWithTitle:@"Take a Picture" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+    if ([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera]) {
+      self.picker.sourceType = UIImagePickerControllerSourceTypeCamera;
+      [self presentViewController:self.picker animated:true completion:nil];
+    }
+  }];
+  
+  [self.imageSelector addAction:chooseImageFromPhotoLibrary];
+  [self.imageSelector addAction:takePicture];
+  [self.imageSelector addAction:cancelAction];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -34,5 +64,19 @@
     // Pass the selected object to the new view controller.
 }
 */
+
+#pragma mark - UIImagePickerControllerDelegate and UINavigationControllerDelegate
+
+-(void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary<NSString *,id> *)info {
+  UIImage *image = info[UIImagePickerControllerOriginalImage];
+  //[self.editUser setProfilePicture:image];
+  [self.editProfileImageButton setImage:image forState:UIControlStateNormal];
+//  [self.editProfileImageButton setBackgroundImage:image forState:UIControlStateNormal];
+  [self.picker dismissViewControllerAnimated:true completion:nil];
+}
+
+-(void) imagePickerControllerDidCancel:(UIImagePickerController *)picker {
+  [self.picker dismissViewControllerAnimated:true completion:nil];
+}
 
 @end
